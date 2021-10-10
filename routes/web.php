c0bf6175eee1f1e0338f2458f9a44ae93927ea71
Bus1k <busiki@interia.pl>
2021-10-10 11:18:47 +0200

    load category and author in existing post model

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,13 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('/categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author'])
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
-//testtest
+
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts->load(['category', 'author'])
     ]);
 });
