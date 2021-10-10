badded61650bcb399f82a3c9be568977c2b69db7
Bus1k <busiki@interia.pl>
2021-10-10 20:39:50 +0200

    create components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,42 +1,13 @@
 <x-layout>
         @include('_posts-header')
-{{--    @foreach($posts as $post)--}}
-{{--        <article>--}}
-
-{{--            <h1>--}}
-{{--                <a href="/posts/{{ $post->slug }}">--}}
-{{--                    {{ $post->title }}--}}
-{{--                </a>--}}
-{{--            </h1>--}}
-
-{{--            <p>--}}
-{{--                By--}}
-{{--                <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} </a>--}}
-{{--                in--}}
-{{--                <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>--}}
-{{--            </p>--}}
-
-{{--            <div>--}}
-{{--                <p>{{ $post->excerpt }}</p>--}}
-{{--            </div>--}}
-
-{{--        </article>--}}
-{{--    @endforeach--}}
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-            <x-post-featured-card />
-
-            <div class="lg:grid lg:grid-cols-2">
-                <x-post-card />
-                <x-post-card />
-            </div>
-
-            <div class="lg:grid lg:grid-cols-3">
-                <x-post-card />
-                <x-post-card />
-                <x-post-card />
-            </div>
+            @if($posts->count())
+                <x-posts-grid :posts="$posts" />
+            @else
+                <p class="text-center">No posts yet. Please check back later.</p>
+            @endif
         </main>
 
 </x-layout>
