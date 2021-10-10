b160beb1d9c6e82722f66b47837920745196a4bf
Bus1k <busiki@interia.pl>
2021-10-10 13:09:36 +0200

    layout base structure tailwind

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,42 @@
 <x-layout>
+        @include('_posts-header')
+{{--    @foreach($posts as $post)--}}
+{{--        <article>--}}
 
-    @foreach($posts as $post)
-        <article>
+{{--            <h1>--}}
+{{--                <a href="/posts/{{ $post->slug }}">--}}
+{{--                    {{ $post->title }}--}}
+{{--                </a>--}}
+{{--            </h1>--}}
 
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
+{{--            <p>--}}
+{{--                By--}}
+{{--                <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} </a>--}}
+{{--                in--}}
+{{--                <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>--}}
+{{--            </p>--}}
 
-            <p>
-                By
-                <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }} </a>
-                in
-                <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a>
-            </p>
+{{--            <div>--}}
+{{--                <p>{{ $post->excerpt }}</p>--}}
+{{--            </div>--}}
 
-            <div>
-                <p>{{ $post->excerpt }}</p>
+{{--        </article>--}}
+{{--    @endforeach--}}
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+            <x-post-featured-card />
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card />
+                <x-post-card />
             </div>
 
-        </article>
-    @endforeach
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card />
+                <x-post-card />
+                <x-post-card />
+            </div>
+        </main>
+
 </x-layout>
