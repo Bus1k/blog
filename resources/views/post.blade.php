e46e05f7c334e2881f88c1397aa8c509ace45c2a
Bus1k <busiki@interia.pl>
2021-10-11 19:03:19 +0200

    create category label component

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -36,9 +36,7 @@
                     </a>
 
                     <div class="space-x-2">
-                        <a href="/categories/{{ $post->category->slug }}"
-                           class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                           style="font-size: 10px">{{ $post->category->name }}</a>
+                        <x-category-label :category="$post->category" />
                     </div>
                 </div>
 
