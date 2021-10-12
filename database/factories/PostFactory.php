292740228dee1fbf14f5f40efab64c36baeac05a
Bus1k <busiki@interia.pl>
2021-10-12 21:18:51 +0200

    clean

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -26,10 +26,10 @@ class PostFactory extends Factory
         return [
             'user_id'     => User::factory(),
             'category_id' => Category::factory(),
-            'title'       => $this->faker->sentence,
-            'slug'        => $this->faker->slug,
-            'excerpt'     => $this->faker->sentence,
-            'body'        => $this->faker->paragraph
+            'title'       => $this->faker->sentence(),
+            'slug'        => $this->faker->slug(),
+            'excerpt'     => $this->faker->sentence(),
+            'body'        => $this->faker->paragraph()
         ];
     }
 }
