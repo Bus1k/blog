65f0a582db321842a8922944ea478b12227b311c
Bus1k <busiki@interia.pl>
2021-10-12 20:49:54 +0200

    separate other dropdown elements to components

--- resources/views/partials/_posts-header.blade.php
+++ resources/views/partials/_posts-header.blade.php
@@ -19,28 +19,19 @@
                     <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
-                        <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                             height="22" viewBox="0 0 22 22">
-                            <g fill="none" fill-rule="evenodd">
-                                <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                </path>
-                                <path fill="#222"
-                                      d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                            </g>
-                        </svg>
+                        <x-svg-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
                     </button>
                 </x-slot>
 
-                <a href="/"
-                   class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white">All</a>
+                <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
                 @foreach($categories as $category)
-                    <a href="/categories/{{ $category->slug }}"
-                       class="
-                            block text-left px-3 text-sm leading-6
-                            hover:bg-blue-500 focus:bg-blue-500 hover:text-white
-                            {{ isset($currentCategory) && $currentCategory->id === $category->id ? 'bg-blue-500 text-white' : '' }}
-                           ">{{ ucwords($category->name) }}</a>
+                    <x-dropdown-item
+                        href="/categories/{{ $category->slug }}"
+                        :active="isset($currentCategory) && $currentCategory->id === $category->id"
+                    >
+                        {{ ucwords($category->name) }}
+                    </x-dropdown-item>
                 @endforeach
             </x-dropdown>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         'posts'      => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
