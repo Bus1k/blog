27b2ad35f8362a534a1b7bc933aa6d8685c7a320
Bus1k <busiki@interia.pl>
2021-10-25 21:51:53 +0200

    add post component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto asperiores at
+            beatae commodi consectetur eum eveniet expedita in, ipsum labore maxime minus molestiae nam
+            natus nemo nihil perferendis possimus quasi qui quis quo reiciendis saepe ut vero!
+            Accusantium alias aliquid architecto culpa debitis exercitationem facilis libero magni
+        </p>
+    </div>
+</article>
