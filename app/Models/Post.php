f1cf25089afa2654d3ffe59364bea8e48322eb9a
Bus1k <busiki@interia.pl>
2021-10-29 20:25:07 +0200

    create comment model controller and factory, extract panel to separate component

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -34,6 +35,11 @@ class Post extends Model
                 $query->where('username', $filters['author']);
             });
         }
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 
     public function category(): BelongsTo
