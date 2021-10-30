7f6aad286bd72139836ee8609da8191d040dc503
Bus1k <busiki@interia.pl>
2021-10-30 17:18:41 +0200

    Newsletter interface

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -2,36 +2,7 @@
 
 namespace App\Services;
 
-use \MailchimpMarketing\ApiClient;
-
-class Newsletter
+interface Newsletter
 {
-    private ApiClient $mailchimp;
-    private string $subscribersList;
-
-    public function __construct()
-    {
-        $this->mailchimp = new ApiClient();
-        $this->setConfig();
-    }
-
-    private function setConfig(): void
-    {
-        $this->subscribersList = config('services.mailchimp.lists.subscribers');
-
-        $this->mailchimp->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => config('services.mailchimp.prefix')
-        ]);
-    }
-
-    public function subscribe(string $email, string $list = null)
-    {
-        $list ??= $this->subscribersList;
-
-        return $this->mailchimp->lists->addListMember($list, [
-            'email_address' => $email,
-            'status'        => 'subscribed'
-        ]);
-    }
+    public function subscribe(string $email, string $list = null);
 }
