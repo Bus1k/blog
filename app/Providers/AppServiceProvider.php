1ee7aa775c7e4b4afa09c5b920b5a0c6e70847e1
Bus1k <busiki@interia.pl>
2021-10-30 17:19:11 +0200

    register Mailchimp in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsletter;
+use App\Services\Newsletter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            return new MailchimpNewsletter();
+        });
     }
 
     /**
