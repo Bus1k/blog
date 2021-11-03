7a917b7921ecb0101c6398589c46091b6508925a
Bus1k <busiki@interia.pl>
2021-11-03 22:19:00 +0100

    add create form, routing and admin middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -20,6 +21,11 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create', []);
     }
 
 }
---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MustBeAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->guest()) {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        if(auth()->user()->username !== 'busik') {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+}
